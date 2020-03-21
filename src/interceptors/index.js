7ccd11ff0d9495a6b2a9df1c6364baf2a602313d
Duy <vietduy1512@gmail.com>
2020-03-21 12:19:37 +0700

    Update deploy

--- src/interceptors/index.js
+++ src/interceptors/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = process.env.API_URL || 'http://localhost:8080';
+axios.defaults.baseURL = process.env.API_URL;
 axios.defaults.withCredentials = true;
 
 // TODO
