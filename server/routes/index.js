66d3227efb85e036a8e9ffb4e639246e7f60adf5
Duy <vietduy1512@gmail.com>
2020-03-21 10:55:51 +0700

    Seperate be project

--- server/routes/index.js
+++
@@ -1,7 +0,0 @@
-const indexRouter = require('../src/components/home/index.route');
-const authRouter = require('../src/components/auth/auth.route');
-
-module.exports = (app) => {
-  app.use('/', indexRouter);
-  app.use('/', authRouter);
-}
