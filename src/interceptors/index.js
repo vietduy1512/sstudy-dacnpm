91bfef99f11ee936fc1f1a0c5059dd348fa1309c
Duy <vietduy1512@gmail.com>
2020-05-01 12:58:52 +0700

    Update env

--- src/interceptors/index.js
+++ src/interceptors/index.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 import {REACT_APP_API_ENDPOINT} from 'react-native-dotenv';
 
-axios.defaults.baseURL = REACT_APP_API_ENDPOINT || 'http://192.168.0.110:8080';
+axios.defaults.baseURL = REACT_APP_API_ENDPOINT || 'https://dacnpm2020-backend.herokuapp.com/';
 axios.defaults.withCredentials = true;
 
 // TODO
 // axios.interceptors.request
-
 // axios.interceptors.response
