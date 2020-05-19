e23151f2ab12bfd383edc8587ddc4f7b3223565c
Duy <vietduy1512@gmail.com>
2020-05-20 00:23:41 +0700

    Fix bug

--- src/screens/parent/location/ChildLocationScreen.js
+++ src/screens/parent/location/ChildLocationScreen.js
@@ -30,7 +30,7 @@ const ChildLocationScreen = props => {
 
   useEffect(() => {
     setupDefaultRegion();
-    //getChildLocation();
+    getChildLocation();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
